624374fec1065b9d51eaa41b209301e52ddc463b
Meng Chen <cm369575409@qq.com>
2017-03-28 09:12:19 +0800

    Update error.go

--- error.go
+++ error.go
@@ -5,5 +5,5 @@ import "fmt"
 type protocolError string
 
 func (pe protocolError) Error() string {
-	return fmt.Sprintf("redigo: %s (possible server error or unsupported concurrent read by application)", string(pe))
+	return fmt.Sprintf("proxy: %s (possible server error or unsupported concurrent read by application)", string(pe))
 }
